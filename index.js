7850dd7ecf88c8ec30c243f241550dac47acd937
Michael Iodice <miodice3@gmail.com>
2021-01-07 12:24:48 -0700

    Done.

--- index.js
+++ index.js
@@ -14,22 +14,15 @@ function handleClick(e) {
 
 function greet(string){
     let hrs = parseInt(string.split(":"))
-//    let min = parseInt(string.split(":")[1])
       if ( hrs <= 11 ){
-        console.log("Good Morning")
         return "Good Morning"        
       } else if (hrs <= 16){
-        console.log("Good Afternoon")
         return "Good Afternoon"
       } else {
-        console.log("Good Evening")
         return "Good Evening"
       }
-    // console.log(hrs)
-//    console.log(min)
 }
 
 function displayMessage(string){
-    let tag = document.getElementById('greeting')
-    tag.innerText = string
+    document.getElementById('greeting').innerText = string
 }
